0801c3e57787c572fb4fc2e4db4ca42783b701e0
ahmed hassan <ahmed.hassan@brightskiesinc.com>
2025-01-06 21:14:35 +0200

    Finish main layout

---
+++ components/shared/header/index.tsx
@@ -0,0 +1,58 @@
+import Link from "next/link"
+// import { getAllCategories } from '@/lib/actions/product.actions'
+import Menu from "./menu"
+import Search from "./search"
+// import Sidebar from './sidebar'
+// import { getSetting } from '@/lib/actions/setting.actions'
+import { HEADER_MENUS } from "@/lib/ecom/consts/header-menus"
+import { getTranslations } from "next-intl/server"
+
+export const Header = async () => {
+  //   const categories = await getAllCategories()
+  //   const { site } = await getSetting()
+  const t = await getTranslations()
+  return (
+    <header className="bg-black  text-white">
+      <div className="px-2">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
+            <Link
+              href="/"
+              className="flex items-center header-button font-extrabold text-2xl m-1 "
+            >
+              {/* <Image
+                src={site.logo}
+                width={40}
+                height={40}
+                alt={`${site.name} logo`}
+              />
+              {site.name} */}
+            </Link>
+          </div>
+
+          <div className="hidden md:block flex-1 max-w-xl">
+            <Search />
+          </div>
+          <Menu />
+        </div>
+        <div className="md:hidden block py-2">
+          <Search />
+        </div>
+      </div>
+      <div className="flex items-center px-3 mb-[1px]  bg-gray-800">
+        {/* <Sidebar categories={categories} /> */}
+        <div className="flex items-center flex-wrap gap-3 overflow-hidden   max-h-[42px]">
+          {HEADER_MENUS.map(menu => (
+            <Link
+              href={menu.href}
+              key={menu.href}
+              className="header-button !p-2 "
+            >
+              {t("Header." + menu.name)}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </header>
+  )
+}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
+import createNextIntlPlugin from "next-intl/plugin"
+const withNextIntl = createNextIntlPlugin()
 
-const nextConfig: NextConfig = {
+const nextConfig: NextConfig = withNextIntl({
   /* config options here */
-};
+})
 
-export default nextConfig;
+export default nextConfig
